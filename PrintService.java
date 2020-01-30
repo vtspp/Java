3262f818eae6156ac61faaca0231ac3d86b81425
vtspp <49285474+vtspp@users.noreply.github.com>
2020-01-29 23:01:21 -0300

    Classe PrintService

---
+++ PrintService.java
@@ -0,0 +1,27 @@
+package entities.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService <T> {
+	
+	List<T> list = new ArrayList <>();
+	
+	public void addValue(T value) {
+		list.add(value);
+	}
+	
+	public T first () {
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List is empty.");
+		}else {
+			return list.get(0);
+		}
+	
+	}
+	
+	public void print() {
+		System.out.println(list);
+	}
+
+}
