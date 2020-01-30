1c57639b61bd2cb556f8ca2c6ddf13d73b1cc1b0
vtspp <49285474+vtspp@users.noreply.github.com>
2020-01-29 22:57:02 -0300

    Classe Students

---
+++ Students.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Students {
+
+	private Integer id;
+
+	public Students() {
+	}
+
+	public Students(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
